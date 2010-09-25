6473d31d744caca587bd252f57883078711d5eb8
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-09-25 13:07:31 +0200

    Add script.

---
+++ build.sh
@@ -0,0 +1,8 @@
+BASE=http://tindzk.github.com
+
+cp ../docsgen/tpl/style.css style.css
+
+../docsgen/docsgen.bin ../Jivai/manual.tyo       $BASE > Jivai/index.html
+../docsgen/docsgen.bin ../tplgen/manual.tyo      $BASE > tplgen/index.html
+../docsgen/docsgen.bin ../docsgen/manual.tyo     $BASE > docsgen/index.html
+../docsgen/docsgen.bin ../Jivai-Utils/manual.tyo $BASE > Jivai-Utils/index.html
