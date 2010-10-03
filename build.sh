dfe0c044c522b917f15e5c205c8539fdb0fd8bae
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-10-03 19:36:40 +0200

    Add podfm documentation.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ BASE=http://tindzk.github.com
 
 cp ../docsgen/tpl/style.css style.css
 
+../docsgen/docsgen.bin ../podfm/manual.tyo       $BASE > podfm/index.html
 ../docsgen/docsgen.bin ../Jivai/manual.tyo       $BASE > Jivai/index.html
 ../docsgen/docsgen.bin ../tplgen/manual.tyo      $BASE > tplgen/index.html
 ../docsgen/docsgen.bin ../docsgen/manual.tyo     $BASE > docsgen/index.html
