27bd287c95dd6bf8e7988ad8fab0ca481e6e99f7
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-10-31 20:07:41 +0100

    Add thesaurus.

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ BASE=..
 
 cp ../docsgen/tpl/style.css style.css
 
-../docsgen/docsgen.bin ../podfm/manual.tyo       $BASE > podfm/index.html
-../docsgen/docsgen.bin ../Jivai/manual.tyo       $BASE > Jivai/index.html
-../docsgen/docsgen.bin ../tplgen/manual.tyo      $BASE > tplgen/index.html
-../docsgen/docsgen.bin ../docsgen/manual.tyo     $BASE > docsgen/index.html
-../docsgen/docsgen.bin ../Jivai-Utils/manual.tyo $BASE > Jivai-Utils/index.html
+../docsgen/docsgen.bin ../podfm/manual.tyo        $BASE > podfm/index.html
+../docsgen/docsgen.bin ../Jivai/manual.tyo        $BASE > Jivai/index.html
+../docsgen/docsgen.bin ../tplgen/manual.tyo       $BASE > tplgen/index.html
+../docsgen/docsgen.bin ../docsgen/manual.tyo      $BASE > docsgen/index.html
+../docsgen/docsgen.bin ../Jivai-Utils/manual.tyo  $BASE > Jivai-Utils/index.html
+../docsgen/docsgen.bin ../thesaurus/thesaurus.tyo $BASE > thesaurus/index.html
