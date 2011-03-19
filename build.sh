8ff0158537ba3369645424bb71d42fc95b8451c7
Tim Nieradzik <tim.nieradzik@gmx.de>
2011-03-19 23:46:29 +0100

    Update.

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ cp ../docsgen/Templates/style.css style.css
 ../docsgen/docsgen.bin ../Jivai/manual.tyo        $BASE > Jivai/index.html
 ../docsgen/docsgen.bin ../tplgen/manual.tyo       $BASE > tplgen/index.html
 ../docsgen/docsgen.bin ../docsgen/manual.tyo      $BASE > docsgen/index.html
-../docsgen/docsgen.bin ../Jivai-Utils/manual.tyo  $BASE > Jivai-Utils/index.html
+../docsgen/docsgen.bin ../Depend/manual.tyo       $BASE > Depend/index.html
+cp                     ../Depend/screenshot.png           Depend
 ../docsgen/docsgen.bin ../thesaurus/thesaurus.tyo $BASE > thesaurus/index.html
