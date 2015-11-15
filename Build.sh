f6335a47eb06921d2c8c7894b8ec3bb70fee2d06
Tim Nieradzik <tim@metastack.pl>
2015-11-15 22:17:18 +0100

    Uncommitted changes

--- Build.sh
+++ Build.sh
@@ -5,6 +5,7 @@ BASE=..
 cp ../docsgen/Templates/style.css style.css
 
 ../docsgen/docsgen.bin ../podfm/manual.tyo        $BASE > podfm/index.html
+../docsgen/docsgen.bin ../Debit/manual.tyo        $BASE > Debit/index.html
 ../docsgen/docsgen.bin ../Jivai/manual.tyo        $BASE > Jivai/index.html
 ../docsgen/docsgen.bin ../tplgen/manual.tyo       $BASE > tplgen/index.html
 ../docsgen/docsgen.bin ../docsgen/manual.tyo      $BASE > docsgen/index.html
